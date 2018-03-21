310e0b3f67adebf44f587f5cbb6b03d39179d153
Ishano <iro4@miami.edu>
2018-03-21 13:08:29 -0400

    new

--- examples/assignment 4/js/script.js
+++ examples/assignment 4/js/script.js
@@ -3,4 +3,7 @@ $(function(){
   $('#title').on('mouseover', function(){
     $('.fa-arrow-alt-circle-left').toggleClass('fa-arrow-alt-circle-right');
   });
+  $('#title').on('mouseleave', function(){
+    $('.fa-arrow-alt-circle-left').toggleClass('fa-arrow-alt-circle-right');
+  });
 });
